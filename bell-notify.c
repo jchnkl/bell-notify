5074cd329461fc939b1f9cc1586b6bbe7de1da78
Jochen Keil <jochen.keil@gmail.com>
2015-06-16 11:42:18 +0200

    Minimize imports

--- bell-notify.c
+++ bell-notify.c
@@ -1,15 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <sys/time.h>
-#include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
 #include <pty.h>
 #include <poll.h>
 #include <canberra.h>
